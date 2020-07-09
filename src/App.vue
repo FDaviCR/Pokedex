c561f19a00df786398cda70b1ba377f727b88f5d
Davi <davicamiloribeiro@gmail.com>
2020-07-08 23:51:03 -0300

    Campo de busca

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
-    <div v-for="(poke,index) in pokemons" :key="index">
-      <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+    <h1 class="is-size-1">Pokédex</h1>
+    <div class="column is-half is-offset-one-quarter">
+      <input type="text" name="" id="" placeholder="Pesquisar pokemon pelo nome" v-model="busca" class="input is-rounded">
+      <button id="buscaBtn" class="button is-fullwidth is-success" @click="buscar">Buscar</button>
+      <div v-for="(poke,index) in filteredPokemons" :key="poke.url">
+        <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+      </div>
     </div>
   </div>
 </template>
@@ -14,21 +19,53 @@ export default {
   name: 'App',
   data(){
     return{
-      pokemons: []
+      pokemons: [],
+      filteredPokemons: [],
+      busca: ''
     }
   },
   created: function(){
     axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0').then(res => {
       this.pokemons = res.data.results;
+      this.filteredPokemons = res.data.results;
     })
   },
   components:{
     Pokemon
+  },
+  methods:{
+    buscar: function(){
+      this.filteredPokemons = this.pokemons;
+      if(this.busca == '' || this.busca == ' '){
+        this.filteredPokemons = this.pokemons;
+      }else{
+        this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca);
+      }
+    }
+  },
+  computed:{
+    /*
+    resultadoBusca: function(){
+      if(this.busca == '' || this.busca == ' '){
+        return this.pokemons;
+      }else{
+        return this.pokemons.filter(pokemon => pokemon.name == this.busca);
+      }
+    }
+    */
   }
 }
 </script>
 
 <style>
+body {
+  background-color: #2c3e50;
+}
+
+h1{
+  color: white;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -37,4 +74,9 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+#buscaBtn{
+  margin-top: 1%;
+}
+
 </style>
